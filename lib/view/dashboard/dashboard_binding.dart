6b80c16c1faa474fe4ce28a893f49e5ca9ac0c84
Karen Cadalo <82696971+Krunxx@users.noreply.github.com>
2023-10-14 17:52:57 +0800

    first commit

---
+++ lib/view/dashboard/dashboard_binding.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:get/get_instance/src/bindings_interface.dart';
+import 'package:my_grocery/controller/dashboard_controller.dart';
+
+
+class DashboardBinding extends Bindings{
+  @override
+  void dependencies() {
+    Get.put(DashboardController());
+  }
+}
